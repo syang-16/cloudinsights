e23b36f242fe74804d456ee618238a93a3e63c32
netapp-alavoie <39956590+netapp-alavoie@users.noreply.github.com>
2022-04-05 14:42:18 +0000

    Deploying to gh-pages from  @ 7d61339ba2915a70e578792a6b9462e2556d7aa9 🚀

--- js/pdf-ux.js
+++ js/pdf-ux.js
@@ -2,7 +2,14 @@ $(document).ready(function () {
   try {
     // intialize config variables
     let zipLevel = "";
-    let baseUrlSection = "/us-en/cloudinsights/pdfs/sidebar/";
+
+    let baseUrlSection = "/us-en/cloudinsights/pdfs/";
+    const flavor = $("body").data("flavor");
+    if (flavor) {
+      baseUrlSection += flavor + "/";
+    }
+    baseUrlSection += "sidebar/";
+
     let zipFilename = "";
     let zipFileSize = 0;
     zipLevel = zipLevel !== ""?zipLevel:1;
